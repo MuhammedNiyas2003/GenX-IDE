b16e28f9f68ea036c02579b9f1668ae468b7d578
mohammedfarisofficial <mohammedfarisofficial@gmail.com>
2023-09-28 13:42:24 +0530

    webapp setup

---
+++ genx-ide-web/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="tape">
+        <span>GenX IDE (Under Construction)</span>
+      </h1>
+    </>
+  );
+}
+
+export default App;
