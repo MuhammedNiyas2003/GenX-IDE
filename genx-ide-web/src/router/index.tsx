012dc374792ea107b9dcbaf38605fd7d57ac4ea5
mohammedfarisofficial <mohammedfarisofficial@gmail.com>
2023-10-03 16:57:16 +0530

    router bug fix

--- genx-ide-web/src/router/index.tsx
+++ genx-ide-web/src/router/index.tsx
@@ -19,8 +19,8 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
       <Route path="/" element={<UnAuthenticatedLayout />}>
+        <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
       </Route>
       <Route path="/" element={<ExploreLayout />}>
         <Route path="/explore" element={<Explore />} />
