8f3dc93d650179f6c79bf548be4768a832733d08
mohammedfarisofficial <mohammedfarisofficial@gmail.com>
2024-01-13 13:53:07 +0530

    compiler cors checking:development

--- genx-ide-web/src/App.tsx
+++ genx-ide-web/src/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import router from "./router";
 
+
 function App() {
   return <RouterProvider router={router} />;
 }
--- genx-ide-web/src/components/Workspace/Compiler/index.tsx
+++ genx-ide-web/src/components/Workspace/Compiler/index.tsx
@@ -1,6 +1,44 @@
+import axios from "axios";
 import "./style.scss";
+import { useEffect } from "react";
 
 const Compiler = () => {
+  useEffect(() => {
+    const compiler = async () => {
+      try {
+        // Request body
+        const requestBody = {
+          code: "let a = 'faris'; console.log(a)",
+          input: "",
+          language: "js",
+          save: false,
+        };
+
+        // Request headers
+        const requestHeaders = {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": "true",
+          "Access-Control-Allow-Headers": "content-type",
+        };
+
+        const res = await axios.post(
+          "https://codejudge.geeksforgeeks.org/submit-request",
+          requestBody,
+          {
+            headers: requestHeaders,
+          }
+        );
+
+        console.log(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    compiler();
+  }, []);
+
   return <div className="compiler-container">index</div>;
 };
 
