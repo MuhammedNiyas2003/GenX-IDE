bfa73aec14ee78f8008a83894d7512628f8aa23b
Mohammed Faris <68058442+mohammedfarisofficial@users.noreply.github.com>
2024-02-13 10:34:57 +0530

    Update openai.js
    merge fix

--- server/controllers/openai.js
+++ server/controllers/openai.js
@@ -1,23 +1,10 @@
 import axios from "axios";
-<<<<<<< Updated upstream
-=======
 import dotenv from "dotenv";
 
 dotenv.config();
->>>>>>> Stashed changes
 
 const codeConverter = async (req, res) => {
   const { source, sourceCode, destination } = req.body;
-  // const source = "javascript";
-  // const destination = "python";
-  // const code = 'console.error("hi)"';
-<<<<<<< Updated upstream
-  const API_KEY = "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";
-=======
-  const API_KEY = "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";
-  console.log(process.env.OPENAI_API_KEY);
->>>>>>> Stashed changes
-
   try {
     const response = await axios.post(
       "https://api.openai.com/v1/chat/completions",
@@ -35,8 +22,7 @@ const codeConverter = async (req, res) => {
       {
         headers: {
           "Content-Type": "application/json",
-<<<<<<< Updated upstream
-          Authorization: `Bearer ${API_KEY}`,
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
         },
       }
     );
@@ -44,7 +30,6 @@ const codeConverter = async (req, res) => {
     console.log(generatedCode);
     console.log(response.data.usage);
     res.send(generatedCode)
-=======
           Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
         },
       }
@@ -53,7 +38,6 @@ const codeConverter = async (req, res) => {
     console.log(generatedCode);
     console.log(response.data.usage);
     res.send(generatedCode);
->>>>>>> Stashed changes
   } catch (err) {
     console.log("error here", err.response.data);
     return;
